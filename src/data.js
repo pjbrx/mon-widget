1819362ed2dd0899968147aca50dec9c6f022875
pjbrx <pierrejean.brx@gmail.com>
2025-02-20 18:41:38 +0100

    initialisation du projet. Page de base créée avec ttes entreprise et widget correspondant

---
+++ src/data.js
@@ -0,0 +1,14 @@
+export const assistants = [
+    { id: "lrlluI50KDXyFTccRXwv", name: "ExpansionTV" },
+    { id: "vAVWpiIxaI6uJPpPzRy4", name: "Rentiteasy" },
+    { id: "Nwzu75oxtg6FYOv8xdLS", name: "Exemple" },
+    { id: "", name: "Cléa" },
+    { id: "", name: "Laforgecrypto" },
+    { id: "", name: "Youtube" },
+    { id: "", name: "Tesla" },
+    { id: "", name: "Netflix" },
+    { id: "", name: "Google" },
+
+    // Ajoute ici d'autres assistants au besoin
+  ];
+  
