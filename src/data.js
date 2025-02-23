2df0c70f2e7bab88f19ac787e977f06376e1fbe5
pjbrx <pierrejean.brx@gmail.com>
2025-02-23 16:01:37 +0100

    changements liés à la construction du widget dans le fichier

--- src/data.js
+++ src/data.js
@@ -4,6 +4,7 @@ export const assistants = [
     { id: "vAVWpiIxaI6uJPpPzRy4", name: "Rentiteasy" },
     
     { id: "Nwzu75oxtg6FYOv8xdLS", name: "Exemple" },
+    { id: "uY98U5YsfR4bGJgXlAKE", name: "Cléa" }
      
     // Ajoute ici d'autres assistants au besoin
   ];
